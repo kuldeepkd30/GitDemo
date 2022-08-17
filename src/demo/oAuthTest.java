f7b86a6b3a50a36e4c31021a6b4a4aae1b28cbf8
kuldeepkd30 <dabhi.kuldeep30595@icloud.com>
2022-08-17 12:04:08 -0400

    this is commit from 2nd Workspace

--- src/demo/oAuthTest.java
+++ src/demo/oAuthTest.java
@@ -29,7 +29,8 @@ ChromeOptions options = new ChromeOptions();
 		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Owner\\Pictures\\chromedriver.exe");
 			WebDriver driver= new ChromeDriver(options);
 			
-
+			System.out.println("first");
+			System.out.println("second");
 			driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php&state=verifyfjdss");
 			Thread.sleep(3000);
 			driver.findElement(By.cssSelector("input[type='email']")).sendKeys("srinath19830@gmail.com");
